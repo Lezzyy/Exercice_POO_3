5818200c5735521e6123bf6024137bc5ee13f670
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-08-28 14:01:01 +0200

    restart

--- js/main.js
+++ js/main.js
@@ -16,37 +16,34 @@
 //
 // });
 
-function Descriptif(titre, accroche, description) {
+
+
+// function Form() {
+//   this.elements = function() {
+//     var element = document.getElementsByClassName('inputValue');
+//     var title = document.getElementsByClassName('inputText');
+//     for (i = 0; i <= 2; i++) {
+//       title[i].innerHTML = element[i].value;
+//       console.log(title[i]);
+//     }
+//   }
+// }
+//
+// var product = new Form();
+// var btn = document.getElementById('send');
+// var card = document.getElementsByClassName('fiche');
+// btn.onclick = function() {
+//   product.elements();
+//   card[0].style.display = 'block';
+//   for (i = 0; i <= 2; i++) {
+//     document.getElementsByClassName('inputValue')[i].value = ("");
+//   }
+// }
+
+function clientFile(titre, accroche, description) {
   this.titre = titre;
   this.accroche = accroche;
   this.description = description;
 }
 
-
-function Form() {
-  this.elements = function() {
-    var element = document.getElementsByClassName('inputValue');
-    var title = document.getElementsByClassName('inputText');
-    for (i = 0; i <= 2; i++) {
-      title[i].innerHTML = element[i].value;
-      console.log(title[i]);
-    }
-  }
-
-  // title=elements[0].value,
-}
-// var ficheProduit = {
-//
-// }
-
-// var produit = new Descriptif (document.getElementById('titre').value, document.getElementById('accroche').value, document.getElementById('description').value);
-var product = new Form();
-var btn = document.getElementById('send');
-var card = document.getElementsByClassName('fiche');
-btn.onclick = function() {
-  product.elements();
-  card[0].style.display = 'block';
-  for (i = 0; i <= 2; i++) {
-    document.getElementsByClassName('inputValue')[i].value = ("");
-  }
-}
+// var info = new clientFile();
